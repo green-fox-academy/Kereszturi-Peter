91d7fe7a860e7c962a45f927365f8515503246db
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-11 11:56:43 +0200

    Add task 17

---
+++ week-01/day-04/repos/AverageOfInput/AverageOfInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AverageOfInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double var = 0, temp;
+            Console.WriteLine("Type in 5 numbers: ");
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                temp = Convert.ToInt32(Console.ReadLine());
+                var += temp;
+            }
+
+            Console.WriteLine("Sum: " + var + "\r\nAverage: " + var / 5);
+            Console.ReadLine();
+                
+
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+        }
+    }
+}
