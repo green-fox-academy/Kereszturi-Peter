6d23c70fb56a67614dc82e3ecf8764c91cf0b6f4
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-11 10:04:20 +0200

    Add SecondsInADay

---
+++ week-01/day-04/repos/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+        }
+    }
+}
--- week-01/day-04/repos/SecondsInADay/SecondsInADay/Program.cs
+++ week-01/day-04/repos/SecondsInADay/SecondsInADay/Program.cs
@@ -10,6 +10,12 @@ namespace SecondsInADay
             int currentMinutes = 34;
             int currentSeconds = 42;
 
+            int remainingSeconds = 24 - currentHours * 60 * 60 + 1440 - currentMinutes * 60 + 86400 - currentSeconds;
+
+            Console.WriteLine(remainingSeconds);
+
+            Console.ReadLine();
+
             // Write a program that prints the remaining seconds (as an integer) from a
             // day if the current time is represented by the variable
         }
