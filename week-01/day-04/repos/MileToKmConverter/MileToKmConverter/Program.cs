b10a9faff4616c848c8546e59b575e9643afb3fc
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-11 10:40:35 +0200

    Add task 15

--- week-01/day-04/repos/MileToKmConverter/MileToKmConverter/Program.cs
+++ week-01/day-04/repos/MileToKmConverter/MileToKmConverter/Program.cs
@@ -6,7 +6,12 @@ namespace MileToKmConverter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Type in a distance:");
+            int distance = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine("Your distance in miles is :" + distance * 0.621371192);
+
+            Console.ReadLine();
             // Write a program that asks for an integer that is a distance in kilometers,
             // then it converts that value to miles and prints it
         }
