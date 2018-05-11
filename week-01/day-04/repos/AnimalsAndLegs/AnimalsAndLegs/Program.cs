662465a8e8c400a41f42565c34b3573f4e1a036b
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-11 11:03:21 +0200

    Add task 16

---
+++ week-01/day-04/repos/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many chickens does the farmer have?");
+            int numOfChickens = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many pigs does the farmer have?");
+            int numOfPigs = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("The animals have " + (numOfChickens *2 + numOfPigs * 4) + " legs overall.");
+
+            Console.ReadLine();
+
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The second represents the number of pigs owned by the farmer
+            // It should print how many legs all the animals have
+        }
+    }
+}
