482c827a9a0e4166a850d91459d585276c385a40
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-15 16:57:51 +0200

    Add task 8 (Lists/4)

---
+++ week-02/day-02/repos/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+        // Create a method called AppendA() that adds "a" to every string in the far list.
+        // The parameter should be a list.
+
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            
+
+            Console.WriteLine(AppendA(far));
+
+            Console.ReadLine();
+            // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+        }
+        public static StringBuilder AppendA(List<string> far)
+        {
+            StringBuilder appendFar = new StringBuilder();
+            for (int i = 0; i < far.Count; i++)
+            {
+                appendFar.Append(far[i] + "a ");
+            }
+            return appendFar;
+        }
+    }
+}
---
+++ week-02/day-02/repos/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandyShop
+{
+    public class Candyshop
+    {
+        // Accidentally we added "2" and "false" to the list.
+        // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+        // No, don't just remove the lines
+        // Create a method called Sweets() which takes the list as a parameter.
+
+        public static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+                        
+            Console.WriteLine(Sweets(list));
+
+            Console.ReadLine();
+            // Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+        }
+        public static string Sweets(List<object> sweetList)
+        {
+            sweetList[1].ToString();
+            sweetList[1] = "Croissant";
+
+            sweetList[3].ToString();
+            sweetList[3] = "Ice cream";
+
+            string correctSweets = "";
+
+            foreach (var sweets in sweetList)
+            {
+                correctSweets += sweets + " ";
+            }
+            return correctSweets;
+        }
+    }
+}
