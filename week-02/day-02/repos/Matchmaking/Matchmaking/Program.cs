96c36378eca958e4afee9d91abf6721d1d4df421
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-15 16:02:22 +0200

    Add tasks 6,7

---
+++ week-02/day-02/repos/Matchmaking/Matchmaking/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        // Write a method that joins the two lists by matching one girl with one boy into a new list
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+
+            Console.WriteLine(MakingMatches(girls, boys));
+
+            Console.ReadLine();
+        }
+        public static StringBuilder MakingMatches(List<string> girls, List<string> boys)
+        {
+            StringBuilder boysAndGirls = new StringBuilder();
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                boysAndGirls.Append(girls[i] + " ").Append(boys[i] + " ");
+            }
+            boysAndGirls.Append(boys[5]);
+            return boysAndGirls;
+        }
+    }
+}
---
+++ week-02/day-02/repos/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        // Saturn is missing from the planetList
+        // Insert it into the correct position
+        // Create a method called PutSaturn() which has list parameter and returns the correct list of planets as a string.
+
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+                        
+            Console.WriteLine(PutSaturn(planetList));
+
+            Console.ReadLine();
+            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+        }
+        public static string PutSaturn(List<string> putThePlanetInTheString)
+        {
+            putThePlanetInTheString.Insert(5, "Saturn");
+                        
+            StringBuilder addSaturn = new StringBuilder();
+
+            for (int i = 0; i < putThePlanetInTheString.Count; i++)
+            {
+                addSaturn.Append(putThePlanetInTheString[i] + " ");
+            }
+            return addSaturn.ToString();
+        }
+    }
+}
