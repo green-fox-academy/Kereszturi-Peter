8e5d09e67f6604a65688df9f5ecce410057daffb
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-16 17:03:59 +0200

    Add task CopyFile, Calculator

---
+++ week-02/day-03/repos/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+    // It should take the filenames as parameters
+    // It should return a boolean that shows if the copy was successful
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\greenfox\Plonee\week-02\day-03\repos\CopyFile\CopyFile\FileToCopyFrom.txt";
+            Console.WriteLine(CopyFile(filePath));
+            Console.ReadLine();
+        }
+        public static bool CopyFile(string filename)
+        {
+            string text = File.ReadAllText(filename);
+
+            try
+            {
+                File.WriteAllText(@"D:\greenfox\Plonee\week-02\day-03\repos\CopyFile\CopyFile\FileToCopyTo.txt", text);
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+    }
+}
