863e58c9f02029009119b6ab1b13cb0283f0c0da
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-18 16:31:47 +0200

    Add tasks of today. 1st complete, 2nd needs work

---
+++ week-02/day-05/repos/EnvelopeStar/EnvelopeStar/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EnvelopeStar;
+
+namespace EnvelopeStar
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            TopRightCorner(foxDraw);
+        }
+
+        public static void TopRightCorner(FoxDraw topRight)
+        {
+            double pointOne = 0;
+            double pointTwo = 25;
+
+            for (int i = 0; i < topRight.Canvas.Width / 25; i++)
+            {
+                topRight.StrokeColor(Colors.Green);
+                topRight.DrawLine(pointOne, topRight.Canvas.Width / 2, topRight.Canvas.Height / 2, pointOne);
+
+                pointOne += 25;
+                pointTwo += 25;
+            }
+        }
+    }
+}
---
+++ week-02/day-05/repos/LinePlay/LinePlay/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LinePlay;
+
+namespace LinePlay
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            TopRightPurpleLines(foxDraw);
+            BottomLeftGreenLines(foxDraw);
+        }
+
+        public void TopRightPurpleLines(FoxDraw purpleLines)
+        {
+            double x = 0;
+            double y = 25;
+
+            for (int i = 0; i < canvas.Width / 25; i++)
+            {
+                purpleLines.StrokeColor(Colors.Purple);
+                purpleLines.DrawLine(x, 0, canvas.Height, y);
+                x += 25;
+                y += 25;
+            }
+        }
+
+        public void BottomLeftGreenLines(FoxDraw greenLines)
+        {
+            double x = 0;
+            double y = 25;
+
+            for (int i = 0; i < canvas.Width / 25; i++)
+            {
+                greenLines.StrokeColor(Colors.Green);
+                greenLines.DrawLine(0, x, y, canvas.Height);
+                x += 25;
+                y += 25;
+            }
+        }
+    }
+}
