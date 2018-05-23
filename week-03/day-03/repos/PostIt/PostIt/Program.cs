f67aeaa8c2153a140708742527e8fa63d7e5d257
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-23 12:31:27 +0200

    Add task 1 and 2 of the day

---
+++ week-03/day-03/repos/PostIt/PostIt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt Idea1 = new PostIt("orange", "Idea 1", "blue");
+            PostIt Awesome = new PostIt("pink", "Awesome", "black");
+            PostIt Superb = new PostIt("yellow", "Superb", "green");
+
+            Superb.WriteColor();
+            Awesome.WriteText();
+
+            Console.ReadLine();
+        }
+
+    }
+
+    public struct PostIt
+    {
+        string BackgroundColor;
+        string Text;
+        string TextColor;
+
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            BackgroundColor = backgroundColor;
+            Text = text;
+            TextColor = textColor;
+        }
+
+        public void WriteColor()
+        {
+            Console.WriteLine(BackgroundColor);
+        }
+
+        public void WriteText()
+        {
+            Console.WriteLine(Text);
+        }
+    }
+
+}
