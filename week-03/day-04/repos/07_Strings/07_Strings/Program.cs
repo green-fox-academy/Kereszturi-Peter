d94d3102159f3447cc1f22890d7f477749edf4e2
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-24 17:34:24 +0200

    Add task 07_Strings

---
+++ week-03/day-04/repos/07_Strings/07_Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_Strings
+{
+    class Program
+    {
+        // Given a string, compute recursively (no loops) a new string where all the
+        // lowercase 'x' chars have been changed to 'y' chars.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChangeCharacterX("xoxo"));
+            Console.ReadLine();
+        }
+
+        public static string ChangeCharacterX(string characterToChange)
+        {
+            if (characterToChange.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                return characterToChange.Replace("x", "y") + ChangeCharacterX(characterToChange.Remove(0));
+            }
+        }
+    }
+}
