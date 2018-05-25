b91a0833d0f3569d55d3f08640872d4cb5277d34
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-05-25 16:00:03 +0200

    Add exam task of 1st week. Add week-03 demo. Start Pirates task.

---
+++ week-01/day-05/ParametricAverage/ParametricAverage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametricAverage
+{
+    class Program
+    {
+        // Write a program that asks for a number.
+        // It would ask this many times to enter an integer,
+        // if all the integers are entered, it should print the sum and average of these
+        // integers like:
+        //
+        // Sum: 22, Average: 4.4
+
+        static void Main(string[] args)
+        {
+            int userNumber;
+            double sumOfNumbers = 0;
+            double averageOfNumbers;
+
+            Console.WriteLine("Please enter a number!");
+            userNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter {0} numbers separated by enter!", userNumber);
+
+            for (int i = 0; i < userNumber; i++)
+            {
+                sumOfNumbers += double.Parse(Console.ReadLine());
+            }
+            
+            averageOfNumbers = sumOfNumbers / userNumber;
+
+            Console.WriteLine("\nSum: {0}, Average: {1}", sumOfNumbers, averageOfNumbers);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-04/repos/Pirates/Pirates/PiratesYarr.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+    class PiratesYarr
+    {
+        private string pirateName;
+        private int intoxicationLevel;
+
+        public PiratesYarr(string pirateName)
+        {
+            this.pirateName = pirateName;
+            intoxicationLevel = 0;
+        }
+
+        public int GetIntoxicationLevel()
+        {
+            return intoxicationLevel;
+        }
+
+        public int DrinkSomeRum()
+        {
+            return intoxicationLevel++;
+        }
+
+        public string HowsItGoingMate()
+        {
+            if (DrinkSomeRum() < 4)
+            {
+                return "Pour me anudder";
+            }
+            else
+            {
+                return "Arghh, I'ma Pirate. How d'ya d'ink its goin?";
+            }
+        }
+
+        public string PassOut()
+        {
+            return pirateName + " is passed out!";
+        }
+
+        public void Die()
+        {
+            
+        }
+
+        public void Brawl()
+        {
+
+        }
+    }
+}
