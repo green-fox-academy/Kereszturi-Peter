69848c9f6a0ea52ab0d46b99e5b0b76e9fef1f55
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-18 13:52:20 +0200

    Add excercise4

---
+++ week-06/day-01/repos/Excercise4/Excercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            IEnumerable<int> squaredMoreThan20 = from num in n
+                                                 where num * num > 20
+                                                 select num;
+            foreach (var item in squaredMoreThan20)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerable<int> squarerMoreThan20Lambda = n.Where(x => x * x > 20);
+
+            foreach (var item in squarerMoreThan20Lambda)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
