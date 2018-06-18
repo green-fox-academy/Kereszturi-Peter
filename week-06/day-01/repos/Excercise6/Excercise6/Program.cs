0a5dcebbc80a97e6d280517fd4e96de4463656f6
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-18 14:25:20 +0200

    Add excercise6.

---
+++ week-06/day-01/repos/Excercise6/Excercise6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frequencyOfCharacters = "aaaaassssssdddddddddddddfffff";
+
+            var n = from x in frequencyOfCharacters
+                    group x by x into y
+                    select y;
+
+            foreach (var arrNo in n)
+            {
+                Console.WriteLine(arrNo.Key + " appears " + arrNo.Count() + " times");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
