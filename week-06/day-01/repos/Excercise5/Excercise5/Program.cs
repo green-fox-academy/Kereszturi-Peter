49410d1397b3e8bde7a4ae3d79a3a9dc3ecdaa23
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-18 16:16:38 +0200

    Add excercise7. Change excercise5.

--- week-06/day-01/repos/Excercise5/Excercise5/Program.cs
+++ week-06/day-01/repos/Excercise5/Excercise5/Program.cs
@@ -12,21 +12,17 @@ namespace Excercise5
         {
             int[] arrayOne = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
-            Console.Write("The numbers in the array  are : \n");
-            Console.Write(" 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2\n");
-
             var n = from x in arrayOne
                     group x by x into y
                     select y;
-
-            Console.WriteLine("\nThe number and the Frequency are : \n");
 
             foreach (var arrNo in n)
             {
                 Console.WriteLine("Number " + arrNo.Key + " appears " + arrNo.Count() + " times");
             }
 
-            Console.WriteLine("\n");
+            var frequencyMethod = arrayOne.ToDictionary(x => x.Key, x => x.ToList().Count);
+
             Console.ReadLine();
         }
     }
---
+++ week-06/day-01/repos/Excercise7/Excercise7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var cityNames = from i in cities
+                            where i.StartsWith("A") && i.EndsWith("I")
+                            select i;
+
+            foreach (var item in cityNames)
+            {
+                Console.WriteLine(item);
+            }
+
+            var cityNamesMethod = cities.Where(x => x.StartsWith("A") && x.EndsWith("I"));
+
+            foreach (var item in cityNamesMethod)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
