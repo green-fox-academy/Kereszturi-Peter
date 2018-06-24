70e1aa251c75cd9ba85f23addf90f978603c2e00
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-24 10:48:37 +0200

    Add lot of things

---
+++ week-06/day-04/repos/TODOAPP2/TODOAPP2/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace TODOAPP2
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source = database.sqlite3");
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+            }
+        }
+    }
+}
