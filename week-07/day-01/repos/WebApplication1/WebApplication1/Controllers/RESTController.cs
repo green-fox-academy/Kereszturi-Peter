edaa2abaabdbf433aabaafbd05b98d032f5856e1
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-25 12:37:24 +0200

    feat(WebApplication1) new routing and greeting method.

---
+++ week-07/day-01/repos/WebApplication1/WebApplication1/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("asdf");
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            Greeting konnichiwa = new Greeting();
+            konnichiwa.longID = 1;
+            konnichiwa.content = "Hello World!";
+            return new JsonResult(konnichiwa);
+        }
+    }
+}
---
+++ week-07/day-01/repos/WebApplication1/WebApplication1/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Greeting
+    {
+        public int longID { get; set; }
+        public string content { get; set; }
+    }
+}
