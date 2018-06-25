28c00f7773adc41b48a5012462c5324206707d70
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-25 12:18:48 +0200

    Add ASP dotnet webApp

---
+++ week-07/day-01/repos/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public ContentResult Hello()
+        {
+            return Content("Helloka yoloka!");
+        }
+    }
+
+}
