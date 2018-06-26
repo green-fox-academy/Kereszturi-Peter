2f1cdbd61e7c342181e5d5bcf80675b4019c9c50
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-26 17:36:00 +0200

    new(BankOfSimba) Add parts 1-4

---
+++ week-07/day-02/repos/BankOfSimba/BankOfSimba/Controllers/BankController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using BankOfSimba.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    [Route("bank")]
+    public class BankController : Controller
+    {
+        BankAccountAnimalViewModel baavm = new BankAccountAnimalViewModel()
+        {
+            BankAccountDatabase = new List<BankAccount>()
+            {
+                new BankAccount() {Name = "Simba", Balance = 2000, AnimalType = "lion" },
+                new BankAccount() {Name = "Timon", Balance = 700, AnimalType = "meerkat"},
+                new BankAccount() {Name = "Pumba", Balance = 700, AnimalType = "warthog"},
+                new BankAccount() {Name = "Rafiki", Balance = 2000, AnimalType = "mandrill"},
+                new BankAccount() {Name = "Nala", Balance = 10000, AnimalType = "lion"}
+            }
+        };
+
+        public IActionResult Index()
+        {
+            BankAccount simbasAccount = new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = "Lion"
+            };
+            return View(simbasAccount);
+        }
+
+        [Route("account")]
+        public IActionResult GetAccount()
+        {
+            return View(baavm);
+        }
+
+    }
+}
---
+++ week-07/day-02/repos/BankOfSimba/BankOfSimba/ViewModels/BankAccountAnimalViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BankOfSimba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.ViewModels
+{
+    public class BankAccountAnimalViewModel
+    {
+        public List<BankAccount> BankAccountDatabase { get; set; }
+
+    }
+}
