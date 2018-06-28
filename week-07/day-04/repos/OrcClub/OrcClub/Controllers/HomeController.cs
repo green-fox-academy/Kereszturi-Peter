4f96a49b6313ccc1b373c80a6b592983602da8eb
Peter Keresztúri <peter.kereszuri@outlook.hu>
2018-06-28 12:41:25 +0200

    new(OrcClub) Add routes, orc class

---
+++ week-07/day-04/repos/OrcClub/OrcClub/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrcClub.Models;
+
+namespace OrcClub.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/")]
+        public IActionResult Login(string input)
+        {
+            return Redirect("/Info");
+        }
+
+        [HttpGet]
+        [Route("/Info")]
+        public IActionResult Info()
+        {
+            //object characterName = orc.GetName();
+            return View();
+        }
+    }
+}
